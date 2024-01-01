301af926792919d4739f10bf97c27536344b286a
Mark Brewer <markbrewer369@gmail.com>
2023-12-31 21:26:25 -0600

    parter pages done

---
+++ pages/HomePartnerJournalView/[partnerJournalId].js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import { getJournalByIdWithTags } from '../../API/Promises';
+import MoodTagDisplay from '../../components/MoodTagDisplay';
+
+export default function PartnerJournalDetailsHome() {
+  const [journal, setJournal] = useState({});
+  const router = useRouter();
+  const { partnerJournalId } = router.query;
+  const getPartnerJournal = () => {
+    getJournalByIdWithTags(partnerJournalId)?.then((data) => {
+      setJournal(data);
+    });
+  };
+
+  useEffect(() => {
+    getPartnerJournal();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <>
+      <h1 className="text-center">{journal[4]}</h1>
+      <h5 className="text-center">{journal[5]}</h5>
+      <hr />
+      <h4 className="text-center">What they were feeling</h4>
+      <h1 className="d-flex justify-content-center">{journal[8]?.map((moodTag) => <MoodTagDisplay moodTagObj={moodTag} />)}</h1>
+    </>
+  );
+}
---
+++ pages/PartnerProfile/[PartnerId].js
@@ -0,0 +1,71 @@
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import { Image } from 'react-bootstrap';
+import { getUserById, getUserByUid, getUserWithMyMoodDTO } from '../../API/Promises';
+import { useAuth } from '../../utils/context/authContext';
+
+export default function PartnerProfilePage() {
+  const router = useRouter();
+  const { user } = useAuth();
+  const [currUser, setCurrUser] = useState({});
+  const [partnerUser, setPartnerUser] = useState({});
+  const [partnerMyMoodDto, setPartnerMyMoodDto] = useState({});
+  const { PartnerId } = router.query;
+
+  const getTheCurrentUserAndPartner = () => {
+    getUserByUid(user.uid)?.then((data) => {
+      setCurrUser(data);
+      getUserById(PartnerId)?.then((partnerData) => {
+        setPartnerUser(partnerData);
+      });
+    });
+  };
+
+  const getPartnerUserMood = () => {
+    if (user.id != null) {
+      getUserWithMyMoodDTO(user.id)?.then((data) => {
+        setPartnerMyMoodDto(data);
+      });
+    }
+  };
+  useEffect(() => {
+    getTheCurrentUserAndPartner();
+    getPartnerUserMood();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const formatAnniversaryDate = (dateString) => {
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    const date = new Date(dateString);
+    return date.toLocaleDateString('en-US', options);
+  };
+
+  return (
+    <>
+      <div className="d-flex">
+        <div>
+          <div
+            className="d-flex flex-column justify-content-between"
+            style={{
+              maxWidth: '300px',
+              borderRadius: '30px',
+            }}
+          >
+            <Image className="profile-photo-img" src={`/LoveLinkProfilePhotos/${partnerUser.profilePhoto}`} />
+          </div>
+          <div>
+            <h3>Name: {partnerUser.name}</h3>
+            <h3>Gender: {partnerUser.gender}</h3>
+            <h3>Age: {partnerUser.age}</h3>
+            <h3>Bio: {partnerUser.bio}</h3>
+            <h3>Partner Name: {currUser.name}</h3>
+            <h3>Anniversary Date: {formatAnniversaryDate(partnerUser.anniversaryDate)}</h3>
+          </div>
+        </div>
+        <div>
+          <h1>Partners Mood: {partnerMyMoodDto?.myMood?.moodName}</h1>
+        </div>
+      </div>
+    </>
+  );
+}
